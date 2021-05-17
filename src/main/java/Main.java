82455ba7e55b7a6422591e5b5e71d473fd427b5b
ChabbertBenjamin <b.chabbert@outlook.fr>
2021-05-17 18:06:43 +0200

    feat: (Menu): Benjamin Chabbert: création du menu
    + print du menu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,11 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test");
+		
+		// menu blablabla
+		
+		System.out.println("afficher menu voilà");
+		
 	}
 
 }
