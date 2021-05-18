c518e76dfd73ec76438aa9abdfb8b83ba1f93e58
ChabbertBenjamin <b.chabbert@outlook.fr>
2021-05-19 00:29:53 +0200

    Feat (class Utile): Benjamin Chabbert : Connection à la BDD

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 
-		Utile.getSQLServerConnection();
+		Utile.getSQLServerConnection(); 
 	
 	}
 
