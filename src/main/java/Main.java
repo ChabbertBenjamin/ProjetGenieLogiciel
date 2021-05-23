11a5f5430bad5dbc8cdb891164109e53b9e1f3c0
aichetouH <49098828+aichetouH@users.noreply.github.com>
2021-05-23 15:43:42 +0200

    Add files via upload

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,13 @@
+class Main {
 
-public class Main {
+    public static void main(String[] args){
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("test");
-	}
+
+        
+        Frame1 f=new Frame1();
+       
+    }
+
+
 
 }
