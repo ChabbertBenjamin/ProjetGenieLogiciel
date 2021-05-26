08fa65624449b55360ad0e0ec0b0e9cfd3853fe5
ChabbertBenjamin <b.chabbert@outlook.fr>
2021-05-26 13:59:28 +0200

    Feat(class CreationEmploye) : Benjamin Chabbert: verification du login
    + Verification que le login n'existe pas lorsque l'on essaye de créer un
    compte

--- src/main/java/CreationEmploye.java
+++ src/main/java/CreationEmploye.java
@@ -9,6 +9,7 @@ import java.awt.event.WindowEvent;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
@@ -25,9 +26,9 @@ public class CreationEmploye {
 	private JLabel headerLabel;
 	private JLabel statusLabel;
 	private JPanel controlPanel;
-	private JLabel firstNameEmploye,lastNameEmplye,login,password;
+	private JLabel firstNameEmploye, lastNameEmplye, login, password;
 	private JComboBox<String> listEmploye;
-	GridLayout experimentLayout = new GridLayout(0,2);
+	GridLayout experimentLayout = new GridLayout(0, 2);
 
 	public CreationEmploye() {
 
@@ -65,56 +66,73 @@ public class CreationEmploye {
 		headerLabel.setForeground(Color.white);
 
 		firstNameEmploye = new JLabel("Entrer prénom employé");
-        final JTextField tf2=new JTextField();
-        tf2.setSize(100,40);
+		final JTextField tf2 = new JTextField();
+		tf2.setSize(100, 40);
 
-        lastNameEmplye = new JLabel("Entrer nom employé");
-        final JTextField tf3=new JTextField();
-        tf3.setSize(100,40);
-        
-        login = new JLabel("Entrer login employé");
-        final JTextField tf4=new JTextField();
-        tf2.setSize(100,40);
+		lastNameEmplye = new JLabel("Entrer nom employé");
+		final JTextField tf3 = new JTextField();
+		tf3.setSize(100, 40);
 
-        password = new JLabel("Entrer mot de passe employé");
-        final JTextField tf5=new JTextField();
-        tf3.setSize(100,40);
+		login = new JLabel("Entrer login employé");
+		final JTextField tf4 = new JTextField();
+		tf4.setSize(100, 40);
 
-        String[] items = { "Serveur", "Cuisinier", "Assistant service", "Maitre d'hôtel" };
-        listEmploye = new JComboBox(items);
-        
+		password = new JLabel("Entrer mot de passe employé");
+		final JTextField tf5 = new JTextField();
+		tf5.setSize(100, 40);
 
+		String[] items = { "Serveur", "Cuisinier", "Assistant service", "Maitre d'hôtel" };
+		listEmploye = new JComboBox(items);
 
 		JButton okButton = new JButton("OK");
-		
 
 		okButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				DBConnection con = new DBConnection();
 				PreparedStatement pst;
 				Boolean test = true;
-				 try {
-					if(tf2.getText().equals("") || tf3.getText().equals("") || tf4.getText().equals("") || tf5.getText().equals("")) {
-		            	test = false;
-		            }
-					pst = con.mkDataBase().prepareStatement("INSERT into employe (nom, prenom, login, motdepasse, role) values (?,?,?,?,?)");
+				Boolean alreadyExist = false;
+				ResultSet résultats = null;
+				String requete = "SELECT login FROM employe WHERE login='" + tf4.getText() + "'";
+				
+				
+				try {
+					Statement stmt = con.con.createStatement();
+					résultats = stmt.executeQuery(requete);
+					while (résultats.next() && !alreadyExist) {
+						String login = résultats.getString("login");
+						// Si le login existe déjà
+						if (tf4.getText().equals(login)) {
+							alreadyExist = true;
+						}
+					}
+					if (tf2.getText().equals("") || tf3.getText().equals("") || tf4.getText().equals("")
+							|| tf5.getText().equals("") || alreadyExist) {
+						test = false;
+					}
+					pst = con.mkDataBase().prepareStatement(
+							"INSERT into employe (nom, prenom, login, motdepasse, role) values (?,?,?,?,?)");
 					pst.setString(1, tf3.getText());
-	                pst.setString(2, tf2.getText());
-	                pst.setString(3, tf4.getText());
-	                pst.setString(4, tf5.getText());
-	                pst.setString(5, listEmploye.getSelectedItem().toString());
-	                
-	                if(test) {
-	                   pst.execute();
-		               JOptionPane.showMessageDialog(null, "Création effectué !");
-	                }else {
-	                	JOptionPane.showMessageDialog(null, "Impossible de créer l'employe (certain champs sont peut-être vide)");
-	                }
-	               
-	                tf2.setText("");
-	                tf3.setText("");
-	                tf4.setText("");
-	                tf5.setText("");
+					pst.setString(2, tf2.getText());
+					pst.setString(3, tf4.getText());
+					pst.setString(4, tf5.getText());
+					pst.setString(5, listEmploye.getSelectedItem().toString());
+
+					if (test) {
+						pst.execute();
+						JOptionPane.showMessageDialog(null, "Création effectué !");
+					} else if(alreadyExist) {
+						JOptionPane.showMessageDialog(null,
+								"Impossible de créer l'employe (Le login existe déjà)");
+					}else {
+						JOptionPane.showMessageDialog(null,
+								"Impossible de créer l'employe (certain champs sont peut-être vide)");
+					}
+
+					tf2.setText("");
+					tf3.setText("");
+					tf4.setText("");
+					tf5.setText("");
 				} catch (SQLException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
@@ -122,29 +140,26 @@ public class CreationEmploye {
 
 			}
 		});
-
-
+ 
 		JPanel jp = new JPanel(null);
-	      jp.add(firstNameEmploye);
-	      jp.add(tf2);
-	      jp.add(lastNameEmplye);
-	      jp.add(tf3);
-	      jp.add(login);
-	      jp.add(tf4);
-	      jp.add(password);
-	      jp.add(tf5);
-	      jp.add(listEmploye);
-	     
-	      jp.setSize(500,500);
-	      jp.setLayout(experimentLayout);
-	      //controlPanel.add(listEmploye);
-	      controlPanel.add(jp);
-	      jp.add(okButton);
-	     
-			
+		jp.add(firstNameEmploye);
+		jp.add(tf2);
+		jp.add(lastNameEmplye);
+		jp.add(tf3);
+		jp.add(login);
+		jp.add(tf4);
+		jp.add(password);
+		jp.add(tf5);
+		jp.add(listEmploye);
 
-		  mainFrame.setLocationRelativeTo(null);
-	      mainFrame.setVisible(true);
+		jp.setSize(500, 500);
+		jp.setLayout(experimentLayout);
+		// controlPanel.add(listEmploye);
+		controlPanel.add(jp);
+		jp.add(okButton);
+
+		mainFrame.setLocationRelativeTo(null);
+		mainFrame.setVisible(true);
 
 	}
 }
