6e3c91c0cfab005d11b23cb91b9dbd7c8f073bb5
Asma <lahssini.asma@hotmail.com>
2021-05-31 20:09:07 +0100

    recommit US_08

--- src/main/java/fr/ul/miage/restaurant/menu/direteur/AbstractButtonRecette.java
+++ src/main/java/fr/ul/miage/restaurant/menu/direteur/AbstractButtonRecette.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.MessageFormat;
 
-public  abstract class AbstractButtonRecette extends JButton {
+public  abstract class AbstractButtonRecette extends JButton { 
 	
 	
 	private final InterfaceDirecteur parent;
